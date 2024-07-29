06c2cdf114482c0442f9274cdc13f7078a75c88a
Haris Pathan <156300458+haristrilokn@users.noreply.github.com>
2024-07-29 16:05:47 +0530

    Added colorful animated container

---
+++ example/lib/pages/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:colorful_container/colorful_container.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [ColorfulContainer()],
+        ),
+      ),
+    );
+  }
+}
